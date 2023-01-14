8bb5bbbe4e768cc43b61d89b3ad61192496a3b1f
Ghergut Madalin <madalinghergut@gmail.com>
2023-01-15 00:03:34 +0200

    USER CRUD DONE

--- VetClinic/src/main/java/App/Service/UserServiceImp.java
+++ VetClinic/src/main/java/App/Service/UserServiceImp.java
@@ -4,8 +4,12 @@ import App.Entity.User;
 import App.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserServiceImp implements UserService{
     private final UserRepository userRepository;
@@ -60,5 +64,16 @@ public class UserServiceImp implements UserService{
         }else{
             System.out.println("Not found");
         }
+    }
+    @Override
+    public Optional<User> getUserById(int id) {
+       return userRepository.findById(id);
+    }
+
+    @Override
+    public String getDecodedPassword(String encodedPassword) {
+        byte[] decodedPassword = Base64.getDecoder().decode(encodedPassword);
+        decodedPassword = new String(decodedPassword).getBytes();
+        return new String(decodedPassword);
     }
 }
